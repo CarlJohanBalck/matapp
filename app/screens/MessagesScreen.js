9ea3c977325dd6afd81523e3358b7721b0ab1e27
Carl Johan Balck <carl-johan.balck@tactel.se>
2022-09-08 16:27:02 +0200

    Tutorial template

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
+import ListItemSeparator from '../components/ListItemSeparator';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages= [
+const initialMessages= [
     {
         id: 1,
         title: 'T1',
@@ -18,18 +20,29 @@ const messages= [
     },
 ]
 function MessagesScreen(props) {
+
+    const [messages, setMessages] = useState(initialMessages);
+    const handleDelete = message => {
+        setMessages(messages.filter(m => m.id !== message.id))
+
+        }
     return (
         <Screen>
             <FlatList
                 data={messages}
                 keyExtractor={messages => messages.id.toString()}
-                renderItem={({item}) => 
+                renderItem={({item}) => (
                     <ListItem 
                         title={item.title}
                         subTitle={item.description}
                         image={item.image}
-
-                />} />
+                        onPress={() => console.log("Message selected", item)}
+                        renderRightActions={() => 
+                            <ListItemDeleteAction onPress={() => handleDelete}/>}
+                    />
+                )}
+                ItemSeparatorComponent={ListItemSeparator} 
+            />
         </Screen>
     );
 }
